897fe14982a983845bb0edd60b408a80a0ba5ae6
Kiarash Siahbandi <kiarashsiahbandi@gmail.com>
2022-06-18 19:01:01 +0430

    Add files via upload
    Given number of rows, this program draws inverted Pascal's triangle

---
+++ Inverted_Pascal's_Triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, k = 0;
+	cout << "Enter number of rows :	";
+	cin >> n;
+	n++;
+	cout << "\n";
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n - i; ++j)
+		{
+			cout << "* ";
+		}
+		cout << "\n";
+		k = 0;
+		for (int k = 1; k <= i; k++)
+		{
+			cout << " ";
+		}
+	}
+	return 0;
+}
